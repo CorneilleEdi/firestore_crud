907258053bd1a4fbec9ee4da680923eb4d54e9c4
CorneilleEdi <corneilleayao@gmail.com>
2019-09-05 08:06:06 +0530

    resolve dep

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Firebase Anonymous Auth',
+      title: 'Firebase Firestore',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         primaryColor: Color(0XFF6D3FFF),
